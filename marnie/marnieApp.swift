e8b766ab8c5f5e854713204af9438796c59f7d31
Nursultan Askarbekuly <nursultan@outlook.com>
2022-05-22 11:29:47 +0300

    Initial Commit

---
+++ marnie/marnieApp.swift
@@ -0,0 +1,17 @@
+//
+//  marnieApp.swift
+//  marnie
+//
+//  Created by Daniya on 22/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct marnieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
